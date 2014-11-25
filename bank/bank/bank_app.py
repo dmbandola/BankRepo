133d055d2a253938a8f9944606ee4c13823e96bb
thundervoice <dmbbandola@gmail.com>
2014-11-26 01:23:44 +0800

    edited typos

--- bank/bank/bank_app.py
+++ bank/bank/bank_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask (__name__)
 
 @app.route('/')
@@ -6,4 +6,4 @@ def hello_world():
     return render_template ('index.html')
     
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
