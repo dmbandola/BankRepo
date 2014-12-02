f822e72e62562532268724219a1acf4a4dec29d0
thundervoice <dmbbandola@gmail.com>
2014-12-02 17:46:15 +0800

    files

--- bank/bank_app.py
+++ bank/bank_app.py
@@ -16,4 +16,4 @@ if __name__ == '__main__':
 
     account = Account('1111', 50)
     BANK.add_account(account)
-    cProfile.run('app.run(debug=True)')
+    cProfile.run('app.run(debug=True)', sort='time')
